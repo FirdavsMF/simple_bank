1e901e51379b73b365d630a1826390557339050c
Firdavs <firdavs.2001.fed@gmail.com>
2021-07-02 22:15:16 +0500

    добавить промежуточное  для авторизации

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/FirdavsMF/simple_bank/util"
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
