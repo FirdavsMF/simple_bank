12440542a66f4d59262227d5916e41ed314a6fc7
Firdavs <firdavs.2001.fed@gmail.com>
2021-07-02 22:21:38 +0500

    добавить api для входа в систему и вернуть токен доступа

---
+++ util/config.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// Config хранит всю конфигурацию приложения.
+// Значения читаются viper из файла конфигурации или переменной среды.
+type Config struct {
+	DBDriver            string        `mapstructure:"DB_DRIVER"`
+	DBSource            string        `mapstructure:"DB_SOURCE"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+}
+
+// LoadConfig считывает конфигурацию из файлов или переменных среды.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
