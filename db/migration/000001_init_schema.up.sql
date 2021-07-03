b1692169f0ead61ad76059d44c41a17d83e77af3
Firdavs <firdavs.2001.fed@gmail.com>
2021-07-03 11:27:00 +0500

    создадим уникальний идекс на счот (владелец, валют)

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -37,6 +37,6 @@ CREATE INDEX ON "transfers" ("to_account_id");
 
 CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
-COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+COMMENT ON COLUMN "entries"."amount" IS 'может быть отрицательным или положительным';
 
-COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+COMMENT ON COLUMN "transfers"."amount" IS 'должен быть положительным';
