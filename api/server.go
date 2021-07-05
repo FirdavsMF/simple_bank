4a22d3b7b1ad3336d386d0d5eea79c67248f2018
Firdavs <firdavs.2001.fed@gmail.com>
2021-07-05 19:51:00 +0500

    add unit tests for authorization middleware

---
+++ api/server.go
@@ -0,0 +1,66 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	db "github.com/FirdavsMF/simple_bank/db/sqlc"
+	"github.com/FirdavsMF/simple_bank/token"
+	"github.com/FirdavsMF/simple_bank/util"
+)
+
+// Сервер обслуживает HTTP-запросы для нашей банковской службы
+type Server struct {
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
+}
+
+// NewServer создает новый HTTP-сервер и настраивает маршрутизацию.
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
+	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
+	authRoutes.POST("/accounts", server.createAccount)
+	authRoutes.GET("/accounts/:id", server.getAccount)
+	authRoutes.GET("/accounts", server.listAccounts)
+
+	authRoutes.POST("/transfers", server.createTransfer)
+
+	server.router = router
+}
+
+// Пуск запускает HTTP-сервер по определенному адресу
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
